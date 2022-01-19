434d0ea18f822a79e4aed8c1b2b6038b4f9c039b
Ruslana Blozva <runa.blozva@gmail.com>
2022-01-19 15:15:59 +0200

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,20 @@
+
+
+const decrement = document.querySelector('[data-action="decrement"]');
+const increment = document.querySelector('[data-action="increment"]');
+const counterValue = document.querySelector('#value');
+
+decrement.addEventListener('click', function () {
+    console.log('-1');
+    
+    counterValue.innerText --;
+});
+
+increment.addEventListener('click', function () {
+    console.log('+1');
+
+    counterValue.innerText++;
+});
+
+
+ 
